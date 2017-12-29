bf107c4251a59c68761b35cefbe1892f369e9ff8
Serguei Mokhov <mokhov@cse.concordia.ca>
2017-12-28 22:24:37 -0500

    [splints] add initial constants for use in hash arguments

---
+++ src/perl/fp/v11/SPLINTS/Constants.pm
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+package SPLINTS::Constants;
+
+our
+(
+	$WORKSPACE = 'projectID',
+	$ISSUE = 'mrID',
+	$ISSUE_SUBJECT = 'title',
+	$ISSUE_STATUS = 'status',
+	$CONTACT_FIRST_NAME = '',
+	$CONTACT_LAST_NAME = '',
+	$CONTACT_EMAIL = '',
+	$SUBMITTER = 'submitter',
+	$ASSIGNEES = 'assignees',
+	$PRIORITY_NUMBER = 'priorityNumber',
+	$DESCRIPTION = 'description'
+);
