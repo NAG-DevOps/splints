6c4b1905f9c40e9f6f4c8ac504faad2f757285d0
Adriel Fabella <adriel.fab@gmail.com>
2018-02-25 15:18:13 -0500

    #7 [Update] Adding the initial operations and services that we can retrieve from Foot Prints v12

---
+++ src/java/fp/v12/FootPrints12.java
@@ -0,0 +1,113 @@
+package fp.v12;
+
+public class FootPrints12 implements FootPrints12Interface {
+
+
+    @Override
+    public void createItem() {
+        //To implement
+    }
+
+    @Override
+    public int getItemId() {
+        //To implement
+        return 0;
+    }
+
+    @Override
+    public void editItem() {
+        //To implement
+    }
+
+    @Override
+    public void linkItems() {
+        //To implement
+    }
+
+    @Override
+    public void createTicket() {
+        //To implement
+    }
+
+    @Override
+    public void createTicketAndLinkAssets() {
+        //To implement
+    }
+
+    @Override
+    public String getTicketDetails() {
+        //To implement
+        return null;
+    }
+
+    @Override
+    public void editTicket() {
+        //To implement
+    }
+
+    @Override
+    public void linkTickets() {
+        //To implement
+    }
+
+    @Override
+    public void createContract() {
+        //To implement
+    }
+
+    @Override
+    public void createOrEditContract() {
+        //To implement
+    }
+
+    @Override
+    public String getContactAssociatedTickets() {
+        //To implement
+        return null;
+    }
+
+    @Override
+    public void editContract() {
+        //To implement
+    }
+
+    @Override
+    public void createCI() {
+        //To implement
+    }
+
+    @Override
+    public void editCI() {
+        //To implement
+    }
+
+    @Override
+    public void listContainerDefinitions() {
+        //To implement
+    }
+
+    @Override
+    public void listItemDefinitions() {
+        //To implement
+    }
+
+    @Override
+    public void listFieldDefinitions() {
+        //To implement
+    }
+
+    @Override
+    public void listQuickTemplates() {
+        //To implement
+    }
+
+    @Override
+    public void listSearches() {
+        //To implement
+    }
+
+    @Override
+    public void runSearch() {
+        //To implement
+    }
+}
