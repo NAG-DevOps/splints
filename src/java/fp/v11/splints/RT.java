e8b03f2dea96f8ba88cb9d58499228c0a896ada1
Daniil Karpov <daniil.karpov@hotmail.com>
2018-03-18 16:38:45 -0400

    ADD: RT & BitbucketCloud classes that need to be implemented

---
+++ src/java/fp/v11/splints/RT.java
@@ -0,0 +1,43 @@
+package fp.v11.splints;
+
+import javax.xml.soap.SOAPElement;
+
+import org.w3c.dom.NodeList;
+
+public class RT implements ISplints {
+
+	@Override
+	public String createIssue() {
+		// TODO Need to implement this method as a REST service
+		return null;
+	}
+
+	@Override
+	public void editIssue() {
+		// TODO Need to implement this method as a REST service
+	}
+
+	@Override
+	public NodeList getIssueDetails() {
+		return null;
+	}
+
+	@Override
+	public NodeList linkIssues() {
+		return null;
+	}
+
+	@Override
+	public String getIndent(int num) {
+		return null;
+	}
+
+	@Override
+	public void dumpSOAPElement(SOAPElement el, int indent) throws Exception {
+	}
+
+	@Override
+	public void queryIssues() {
+	}
+
+}
