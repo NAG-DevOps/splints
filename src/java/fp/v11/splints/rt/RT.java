6c43f51fe517ce16cfbf8b6363509d72b960a265
Daniil Karpov <daniil.karpov@hotmail.com>
2018-03-19 23:07:24 -0400

    MOD: edited refactored RT methods using configs & constants

--- src/java/fp/v11/splints/rt/RT.java
+++ src/java/fp/v11/splints/rt/RT.java
@@ -1,30 +1,109 @@
 package fp.v11.splints.rt;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
 import javax.xml.soap.SOAPElement;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.w3c.dom.NodeList;
 
 import fp.v11.splints.ISplints;
 
-@Path("/RT")
+/**
+ * Refactored class RT
+ * @author Daniil Karpov from soen487-w18-team08
+ *
+ */
 public class RT implements ISplints {
 
-	@Override
-	@POST
-	@Path("/createIssue")
 	public String createIssue() {
-		// TODO Need to implement this method as a REST service
-		return null;
+		String uri = Config.BASE_URI + "/ticket/new?user=" + Config.AGENT_USERNAME + "&pass=" + Config.AGENT_PASSWORD;
+		HttpPost httppost = new HttpPost(uri);
+		HttpClient httpclient = HttpClientBuilder.create().build();
+		HttpResponse response = null;
+		
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("id: " + Constants.ID);
+			sb.append("Queue: " + Constants.QUEUE);
+			sb.append("Owner: " + Constants.OWNER);
+			sb.append("Creator: " + Constants.CREATOR);
+			sb.append("Requestor: " + Constants.REQUESTOR);
+			sb.append("CC: " + Constants.CC);
+			sb.append("AdminCc: " + Constants.ADMIN_CC);
+			sb.append("Subject: " + Constants.SUBJECT);
+			sb.append("Status: " + Constants.STATUS);
+			sb.append("Priority: " + Constants.PRIORITY);
+			sb.append("InitialPriority: " + Constants.INITIAL_PRIORITY);
+			sb.append("FinalPriority: " + Constants.FINAL_PRIORITY);
+			sb.append("TimeEstimated: " + Constants.TIME_ESTIMATED);
+			sb.append("Starts: " + Constants.STARTS);
+			sb.append("Due: " + Constants.DUE);
+			sb.append("Text: " + Constants.TEXT);
+			
+			StringBody content = new StringBody(sb.toString(), ContentType.TEXT_PLAIN);
+			MultipartEntityBuilder reqEntity = MultipartEntityBuilder.create();
+			reqEntity.addPart("content", content);
+			httppost.setEntity(reqEntity.build());
+			System.out.println("Request: " + httppost.getRequestLine()+"\n");
+			
+			response = httpclient.execute(httppost);
+			
+		}
+		catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+
+		if (response != null){
+			System.out.println(response.getStatusLine());
+		}
+		
+		HttpEntity responseEntity = response.getEntity();
+		if (responseEntity != null) {
+			return responseEntity.toString();
+		} else {
+			return null;
+		}
 	}
 
-	@Override
-	@PUT
-	@Path("/editIssue")
 	public void editIssue() {
-		// TODO Need to implement this method as a REST service
+		String uri = Config.BASE_URI + "/ticket/" + Constants.TICKET_ID + "/edit?user=" + Config.AGENT_USERNAME + "&pass=" + Config.AGENT_PASSWORD;
+		HttpPost httppost = new HttpPost(uri);
+		HttpClient httpclient = HttpClientBuilder.create().build();
+		HttpResponse response = null;
+		
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("Started: " + Constants.STARTED);
+			sb.append("TimeEstimated: " + Constants.TIME_ESTIMATED);
+			sb.append("TimeWorked: " + Constants.TIME_WORKED);
+			
+			StringBody content = new StringBody(sb.toString(), ContentType.TEXT_PLAIN);
+			MultipartEntityBuilder reqEntity = MultipartEntityBuilder.create();
+			reqEntity.addPart("content", content);
+			httppost.setEntity(reqEntity.build());
+			System.out.println("Request: " + httppost.getRequestLine()+"\n");
+			
+			response = httpclient.execute(httppost);
+			
+		}
+		catch (Exception e){
+			System.out.println(e.getMessage());
+		}
+
+		if (response != null){
+			System.out.println(response.getStatusLine());
+		}
+		
+		HttpEntity responseEntity = response.getEntity();
+		if (responseEntity != null) {
+			System.out.println("Response received: " + responseEntity.toString());
+		} 
 	}
 
 	@Override
