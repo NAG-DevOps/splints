4cd9fc2d8a59c55e11853077acf00931517337e5
anrchen <anr_chen@live.concordia.ca>
2018-03-24 01:47:54 -0400

    Fixed createIssue() editIssue()

--- src/java/github/GitHub.java
+++ src/java/github/GitHub.java
@@ -8,92 +8,91 @@ package github;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.json.*;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonString;
+import javax.xml.soap.SOAPElement;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.NodeList;
 
 import fp.v11.splints.ISplints;
 
 public class GitHub implements ISplints {
+	
 
     @Override
-    public String createIssue(String title, String body, String assignee, 
-    		int milestone, String [] labels, String [] assignees){
-    		
-    		URL base = new URL(Config.API);
-        URL url = new URL(base, Config.WORKSPACE + "issues/");
+	public String createIssue(Map<String, Serializable> content) {
+		URL base;
+		URL url = null;
+		try {
+			base = new URL(Config.API);
+	        url = new URL(base, Config.WORKSPACE + "issues/");
+		} catch (MalformedURLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
     		
         HttpClient httpclient = HttpClients.createDefault();
-        HttpPost httppost = new HttpPost(url);
+        HttpPost httppost = new HttpPost(url.toString());
 
         // Request parameters and other properties.
         List<NameValuePair> params = new ArrayList<NameValuePair>(6);
-        params.add(new BasicNameValuePair("title", title));
-        params.add(new BasicNameValuePair("body", body));
-        params.add(new BasicNameValuePair("assignee", assignee));
-        params.add(new BasicNameValuePair("milestone", milestone));
-        params.add(new BasicNameValuePair("labels", labels));
-        params.add(new BasicNameValuePair("assignees", assignees));
-        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-
-        //Execute and get the response.
-        HttpResponse response = httpclient.execute(httppost);
-        HttpEntity entity = response.getEntity();
-
-        if (entity != null) {
-            InputStream instream = entity.getContent();
-            try {
-                return instream;
-            } finally {
-                instream.close();
-            }
-        }
-        return null;
-    }
-
-    public void editIssue(){
-    		URL base = new URL(Config.API);
-        URL url = new URL(base, Config.WORKSPACE + "issues/"+Constants.ISSUE+"?_HttpMethod=PATCH");
-    		
-        HttpClient httpclient = HttpClients.createDefault();
-        HttpPost httppost = new HttpPost(url);
-
-        // Request parameters and other properties.
-        List<NameValuePair> params = new ArrayList<NameValuePair>(6);
-        params.add(new BasicNameValuePair("title", title));
-        params.add(new BasicNameValuePair("body", body));
-        params.add(new BasicNameValuePair("assignee", assignee));
-        params.add(new BasicNameValuePair("milestone", milestone));
-        params.add(new BasicNameValuePair("labels", labels));
-        params.add(new BasicNameValuePair("assignees", assignees));
-        httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-
-        //Execute and get the response.
-        HttpResponse response = httpclient.execute(httppost);
-        HttpEntity entity = response.getEntity();
-
-        if (entity != null) {
-            InputStream instream = entity.getContent();
-            try {
-                return instream;
-            } finally {
-                instream.close();
-            }
-        }
-        return null;
-    }
-
-    public void getIssueDetails(int piIssueNumber) {
+        params.add(new BasicNameValuePair("title", (String) content.get("title")));
+        params.add(new BasicNameValuePair("body", (String) content.get("body")));
+        params.add(new BasicNameValuePair("assignee", (String) content.get("assignee")));
+        params.add(new BasicNameValuePair("milestone", (String) content.get("milestone")));
+        params.add(new BasicNameValuePair("labels", (String) content.get("labels")));
+        params.add(new BasicNameValuePair("assignees", (String) content.get("assignees")));
         try {
+			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+        //Execute and get the response.
+        try {
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			String responseString = EntityUtils.toString(entity, "UTF-8");
+			System.out.println(responseString);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        return null;
+	}
+
+	@Override
+	public NodeList getIssueDetails(Map<String, Serializable> content) {
+		try {
             URL base = new URL(Config.API);
-            URL url = new URL(base, Config.WORKSPACE + "issues/" + piIssueNumber);
+            URL url = new URL(base, Config.WORKSPACE + "issues/" + content.get("issueNumber"));
             String response = sendGet(url);
             System.out.println(response);
             JsonObject object = getJsonObject(response);
@@ -102,7 +101,81 @@ public class GitHub implements ISplints {
         } catch (MalformedURLException e) {
             System.err.println("ERROR: " + e.getMessage());
         }
-    }
+		return null;
+	}
+
+	@Override
+	public NodeList linkIssues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void editIssue(Map<String, Serializable> content) {
+		URL base;
+		URL url = null;
+		try {
+			base = new URL(Config.API);
+	        url = new URL(base, Config.WORKSPACE + "issues/"+Constants.ISSUE+"?_HttpMethod=PATCH");
+		} catch (MalformedURLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+    		
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(url.toString());
+
+        // Request parameters and other properties.
+        List<NameValuePair> params = new ArrayList<NameValuePair>(6);
+        params.add(new BasicNameValuePair("title", (String) content.get("title")));
+        params.add(new BasicNameValuePair("body", (String) content.get("body")));
+        params.add(new BasicNameValuePair("assignee", (String) content.get("assignee")));
+        params.add(new BasicNameValuePair("milestone", (String) content.get("milestone")));
+        params.add(new BasicNameValuePair("labels", (String) content.get("labels")));
+        params.add(new BasicNameValuePair("assignees", (String) content.get("assignees")));
+        try {
+			httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+
+      //Execute and get the response.
+        try {
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			String responseString = EntityUtils.toString(entity, "UTF-8");
+			System.out.println(responseString);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public String getIndent(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dumpSOAPElement(SOAPElement el, int indent) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void queryIssues() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
     /**
     * This method convert JSon String into JSon Object
@@ -136,5 +209,4 @@ public class GitHub implements ISplints {
         }
         return result.toString();
     }
-
 }
