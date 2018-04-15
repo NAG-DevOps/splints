ae4015f993f1b00ee75127567e99ac90bce3b78b
Daniil Karpov <daniil.karpov@hotmail.com>
2018-04-15 16:50:29 -0400

    Added and refactored RESTful web service for splints.

---
+++ src/java/ws/RESTfulSplintsWebService/src/java/ws/ServiceProviderConstants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+/**
+ * Holds all service type constants
+ * @author Daniil Karpov
+ */
+public class ServiceProviderConstants {
+    
+    protected final static String FP11_SERVICE = "fp11";
+    protected final static String FP12_SERVICE = "fp12";
+    protected final static String RT_SERVICE = "rt";
+    protected final static String GITHUB_SERVICE = "github";
+    protected final static String BITBUCKET_SERVICE = "bitbucket";
+}
