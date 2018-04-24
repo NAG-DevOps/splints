2ca1928de1914e8f692c01ec601b1709dfa700a6
Adriel Fabella <adriel.fab@gmail.com>
2018-04-23 20:06:31 -0400

    [Update] Logging restful web service
    Client was created to visualize get and post requests

---
+++ src/java/RESTfulLoggerServices/src/java/entities/Logs.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author AJ
+ */
+@Entity
+@Table(name = "LOGS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Logs.findAll", query = "SELECT l FROM Logs l"),
+    @NamedQuery(name = "Logs.findById", query = "SELECT l FROM Logs l WHERE l.id = :id")})
+public class Logs implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Lob
+    @Size(max = 32700)
+    @Column(name = "MESSAGE")
+    private String message;
+    @Lob
+    @Size(max = 32700)
+    @Column(name = "CREATED_AT")
+    private String createdAt;
+
+    public Logs() {
+    }
+
+    public Logs(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Logs)) {
+            return false;
+        }
+        Logs other = (Logs) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Logs[ id=" + id + " ]";
+    }
+    
+}
